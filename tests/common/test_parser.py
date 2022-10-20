5df0c2d872bb7477d2b261fa16f71b1009f41c74
bilan604 <bilan604@yahoo.com>
2022-10-20 14:18:27 -0700

    content

--- tests/common/test_parser.py
+++ tests/common/test_parser.py
@@ -4,8 +4,10 @@ from common.parser import Parser
 
 @pytest.mark.parametrize("input,expected", 
     [["1", 1],
+    ["2", 2],
+    ["3", 3],
     ["4", 4]],
-    ids=["case 1", "case 2"])
+    ids=["case 1", "case 2", "case 3", "case 4"])
 def test_parse(input: str, expected: int):
-    actual = Parser.parse(input)
+    actual = Parser.str_to_int(input)
     assert actual == expected
--- tests/common/test_spark.py
+++ tests/common/test_spark.py
@@ -3,13 +3,27 @@ import pytest
 from pyspark.sql import SparkSession, Row
 from pyspark_test import assert_pyspark_df_equal
 
+from pyspark.sql import DataFrame
 from common.spark import inner_join
 
-# fix test cases
+
+df_left = spark_session.createDataFrame([
+        Row(key=1, name="a"),
+        Row(key=2, name="b"),
+    ])
+df_right = spark_session.createDataFrame([
+        Row(key=1, number="001"),
+        Row(key=3, number="003"),
+    ])
+df_expected = spark_session.createDataFrame([
+        Row(key=1, name= "a", number="001"),
+    ])
+
+
+
 @pytest.mark.parametrize("input,expected", 
-    [["1", 1],
-    ["4", 4]],
-    ids=["case 1", "case 2"])
-def test_parse(input: str, expected: int):
-    actual = inner_join(input)
-    assert actual == expected
+    [[df_left, df_right, 1]],
+    ids=["case 1"])
+def test_spark(df_left: DataFrame, df_right: DataFrame, df_expected: DataFrame):
+    df_actual = inner_join(df_left, df_right)
+    assert df_actual == df_expected
